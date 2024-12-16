85097e0e06ad5f44f085f012dc58c4c852ad8229
farizahh <90184606+farizahh@users.noreply.github.com>
2024-12-16 18:34:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,22 +45,22 @@ else:
 
 # Informasi tambahan untuk masing-masing skenario
 st.subheader("Detail Skenario:")
-# Skenario 1
-st.write(f"**Skenario 1: Tingkat Penjualan**")
+# Hasil dari Skenario 1: Berdasarkan tingkat penjualan
+st.subheader(f"Hasil Rekomendasi Berdasarkan Tingkat Penjualan untuk {nama_produk}")
 if produk['tingkat_penjualan'] > 300:
     st.write(f"Produk {nama_produk} **Direkomendasikan** berdasarkan tingkat penjualan.")
 else:
     st.write(f"Produk {nama_produk} **Tidak Direkomendasikan** berdasarkan tingkat penjualan.")
 
-# Skenario 2
-st.write(f"**Skenario 2: BPOM**")
+# Hasil dari Skenario 2: Berdasarkan BPOM
+st.subheader(f"Hasil Rekomendasi Berdasarkan BPOM untuk {nama_produk}")
 if produk['bpom'] == 1:
     st.write(f"Produk {nama_produk} **Direkomendasikan** karena memiliki BPOM.")
 else:
     st.write(f"Produk {nama_produk} **Tidak Direkomendasikan** karena tidak memiliki BPOM.")
 
-# Skenario 3
-st.write(f"**Skenario 3: Rating Shopee**")
+# Hasil dari Skenario 3: Berdasarkan rating Shopee
+st.subheader(f"Hasil Rekomendasi Berdasarkan Rating Shopee untuk {nama_produk}")
 if produk['rate'] >= 4.0:
     st.write(f"Produk {nama_produk} **Direkomendasikan** berdasarkan rating Shopee.")
 else:
